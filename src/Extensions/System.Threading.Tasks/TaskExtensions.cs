97b2a56c9e67b041df1c8f67f90c6636d5acffb5
MALU <contact@andantissimo.jp>
2023-12-10 07:33:31 +0900

    fix warnings

--- src/Extensions/System.Threading.Tasks/TaskExtensions.cs
+++ src/Extensions/System.Threading.Tasks/TaskExtensions.cs
@@ -5,20 +5,19 @@ public static partial class TaskExtensions
     public static async Task WithTimeout(this Task task, TimeSpan timeout, CancellationToken canceled = default)
     {
         if (task == await Task.WhenAny(task, Task.Delay(timeout, canceled)).ConfigureAwait(false))
+        {
             task.Wait(canceled);
-        else if (canceled.IsCancellationRequested)
-            throw new OperationCanceledException(canceled);
-        else
-            throw new TimeoutException();
+            return;
+        }
+        canceled.ThrowIfCancellationRequested();
+        throw new TimeoutException();
     }
 
     public static async Task<T> WithTimeout<T>(this Task<T> task, TimeSpan timeout, CancellationToken canceled = default)
     {
         if (task == await Task.WhenAny(task, Task.Delay(timeout, canceled)).ConfigureAwait(false))
             return task.Result;
-        else if (canceled.IsCancellationRequested)
-            throw new OperationCanceledException(canceled);
-        else
-            throw new TimeoutException();
+        canceled.ThrowIfCancellationRequested();
+        throw new TimeoutException();
     }
 }
